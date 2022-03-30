46d7d36b74299a949e55fd00871199df59e0524f
Ruchigour0718 <95860673+Ruchigour0718@users.noreply.github.com>
2022-03-30 14:26:20 +0530

    Add files via upload
    loginlogout

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Dashboard from './component/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+     Hello
+     <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
